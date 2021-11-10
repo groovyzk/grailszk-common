f8bdc05e8222fab230d415ed88c71b809bfb0b27
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2021-11-09 21:59:01 -0300

    Fix zul loading in war (#6)

--- src/main/java/org/zkoss/web/util/resource/GrailsContentLoader.java
+++ src/main/java/org/zkoss/web/util/resource/GrailsContentLoader.java
@@ -1,21 +1,14 @@
 package org.zkoss.web.util.resource;
 
 import grails.core.GrailsApplication;
-import grails.plugins.GrailsPlugin;
-import grails.plugins.GrailsPluginManager;
-import grails.util.Environment;
 import groovy.lang.Writable;
 import groovy.text.Template;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.grails.core.io.DefaultResourceLocator;
-import org.grails.core.io.SpringResource;
 import org.grails.gsp.GroovyPagesTemplateEngine;
-import org.grails.plugins.BinaryGrailsPlugin;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.zkoss.util.resource.Locator;
@@ -26,10 +19,10 @@ import org.zkoss.zk.ui.metainfo.PageDefinitions;
 import org.zkoss.zk.ui.metainfo.Parser;
 
 import java.io.*;
-import java.net.URI;
 import java.net.URL;
 import java.util.Map;
 
+@SuppressWarnings("deprecation")
 public class GrailsContentLoader extends ResourceLoader<PageDefinition> {
     private static final String GROOVY_PAGES_TEMPLATE_ENGINE = "groovyPagesTemplateEngine";
     private static final String UTF_8_ENCODING = "UTF-8";
@@ -39,67 +32,46 @@ public class GrailsContentLoader extends ResourceLoader<PageDefinition> {
     private final WebApp webApp;
     private final ApplicationContext appCtx;
     private final GrailsApplication grailsApplication;
+    private final ResourceUtils resourceUtils;
     
-    public GrailsContentLoader(final WebApp wapp) {
-        webApp = wapp;
-        final WebApplicationContext ctx = WebApplicationContextUtils
-                .getRequiredWebApplicationContext(webApp.getServletContext());
+    public GrailsContentLoader(final WebApp webApp) {
+        this.webApp = webApp;
+        final WebApplicationContext ctx =
+                WebApplicationContextUtils.getRequiredWebApplicationContext(webApp.getServletContext());
         grailsApplication = ctx.getBean("grailsApplication", GrailsApplication.class);
         appCtx = grailsApplication.getMainContext();
         appCtx.getBean("grailsResourceLocator", DefaultResourceLocator.class);
+        resourceUtils = new ResourceUtils(appCtx);
     }
 
     @Override
     public PageDefinition load(final ResourceInfo si) throws Exception {
-        org.springframework.core.io.Resource springResource;
-        if (!Environment.isWarDeployed()) {
-            final String path = "file:./grails-app/zul" + si.path;
-            springResource = grailsApplication.getMainContext().getResource(path);
-            log.debug(">>> Try Resource ::: " + springResource);
-            if (!springResource.exists()) { // .zul file might be in a subproject or plugin
-                springResource = new ClassPathResource(path.replace("file:./grails-app/", ""));
-                log.debug(">>> Try to find Resource on classpath::: " + springResource);
-                if (!springResource.exists()) {
-                    log.debug("NOT found ::: " + springResource);
-                    springResource = null;
-                }
-            }
+        final org.springframework.core.io.Resource springResource = resourceUtils.getResource(si.path);
 
-            if (springResource != null) {
-                log.debug("Get Spring Resource from: " + springResource);
-            }
-        }
-        else {
-            final URI uri = new File(((ServletContextLocator)si.extra)
-                                .getServletContext()
-                                .getRealPath("WEB-INF") + "/grails-app/zul" + si.path)
-                                .toURI();
-            springResource = grailsApplication.getMainContext().getResource(uri.toString());
-            log.debug("Get Spring Resource in WAR mode from: " + uri.toString());
-        }
-        if (springResource != null) {
+        if (springResource.exists()) {
             log.debug("Load from Spring Resource: " + springResource);
             try {
                 return parse(si.path, springResource, si.extra);
-            }
-            catch (Throwable e) {
+            } catch (Throwable e) {
                 log.debug("Cannot parse ZUL from a Spring Resource", e);
                 throw (Exception) e;
             }
         }
+
         if (si.url != null) {
             log.debug("Load from URL: " + si.url);
             return parse(si.path, si.url, si.extra);
         }
+
         if (!si.file.exists()) {
             log.debug("File " + si.file + " not found");
             return null;
         }
+
         try {
             log.debug("Load from File: " + si.file);
             return parse(si.path, si.file, si.extra);
-        }
-        catch (FileNotFoundException ex) {
+        } catch (FileNotFoundException ex) {
             return null;
         }
     }
