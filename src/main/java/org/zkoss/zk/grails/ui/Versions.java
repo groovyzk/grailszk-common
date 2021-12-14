7f900bb1365f44ec1ccd6a50447e3f2829da32ac
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2021-12-14 19:46:23 -0300

    Add ZK as internal dependency only

--- src/main/java/org/zkoss/zk/grails/ui/Versions.java
+++ src/main/java/org/zkoss/zk/grails/ui/Versions.java
@@ -3,17 +3,15 @@ package org.zkoss.zk.grails.ui;
 import org.zkoss.zk.fn.ZkFns;
 
 public class Versions {
-    private static final String ZK6_VERSION = "6.5";
-    private static final String ZK7_VERSION = "7.0";
-    
+    private static final char ZK6_VERSION = '6';
+
     static void versionValidator() {
         final String zkVersion = ZkFns.getVersion();
-        if (zkVersion.startsWith(Versions.ZK6_VERSION)) {
-            return;
+        final char majorZkVersion = zkVersion.charAt(0);
+        if (majorZkVersion <= ZK6_VERSION) {
+            throw new RuntimeException(
+                "ZK version " + zkVersion + " not supported."
+                + " Support is only granted for versions 6 or above.");
         }
-        if (zkVersion.startsWith(Versions.ZK7_VERSION)) {
-            return;
-        }
-        throw new RuntimeException("ZK version " + zkVersion + " not supported.");
     }
 }
